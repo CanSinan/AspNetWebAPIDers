ad4d06158cb3ca7a1817cad4fecf46c6514e2227
CanSinan <pmsinancan@gmail.com>
2023-11-18 14:15:05 +0300

    4 - DotNET WebAPI 5 Temel Metodun Yazımı ve Entityframework InMemory
    https://www.youtube.com/watch?v=x5PAwgnqy_0&t=12s

---
+++ LMS.Data/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Data.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime? CrearedDate { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ LMS.Data/Entities/LMSDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Data.Entities
+{
+    public class LMSDBContext : DbContext
+    {
+        //public LMSDBContext(DbConnection<LMSDBContext>)
+        //{
+
+        //}
+
+        public DbSet<User> Users { get; set; }
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyLMSDatabase");
+        }
+    }
+}
