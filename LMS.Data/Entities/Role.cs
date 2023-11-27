dd92bf651790f467a8d3f20d6e60fff53c4c0913
CanSinan <pmsinancan@gmail.com>
2023-11-27 18:49:36 +0300

    8- AspNet WebAPI Role Tablosu Eklenmesi
    Youtube Linki = https://www.youtube.com/watch?v=E8jBZjQXWRY&list=PLTLwdny-C3ttHmkuXtDBtaYF0P32vcakI&index=8

---
+++ LMS.Data/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.Data.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ LMS.Data/Migrations/20231127153723_UserRole_Added.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRole_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirsName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CrearedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name", "CrearedDate", "LastUpdatedDate", "IsActive" },
+                values: new object[] { 1, "Admin", DateTime.Now, DateTime.Now, true });
+
+            migrationBuilder.InsertData(
+               table: "Role",
+               columns: new[] { "Id", "Name", "CrearedDate", "LastUpdatedDate", "IsActive" },
+               values: new object[] { 2, "Student", DateTime.Now, DateTime.Now, true });
+
+            migrationBuilder.InsertData(
+               table: "Role",
+               columns: new[] { "Id", "Name", "CrearedDate", "LastUpdatedDate", "IsActive" },
+               values: new object[] { 3, "Teacher", DateTime.Now, DateTime.Now, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirsName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Users");
+        }
+    }
+}
