fff3eedb9e00cd67d79590f24c811b3ae095bbfb
Sinan Can <Sinan51can@gmail.com>
2023-12-19 20:45:09 +0300

    12- AspNET WebAPI Course Entity
    Youtube Link = https://www.youtube.com/watch?v=CVNFtycUhFQ&list=PLTLwdny-C3ttHmkuXtDBtaYF0P32vcakI&index=12

--- AspNETWebAPIDers/Controllers/UserController.cs
+++ AspNETWebAPIDers/Controllers/UserController.cs
@@ -2,60 +2,59 @@
 using LMS.Data.Entities;
 using LMSServices.Models;
 using LMSServices.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNETWebAPIDers.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
 
-        private readonly IUserService _userService;
-        private readonly IMapper _mapper;
+		private readonly IUserService _userService;
+		private readonly IMapper _mapper;
 
-        public UserController(IUserService userService, IMapper mapper)
-        {
-            _userService = userService;
-            _mapper = mapper;
-        }
+		public UserController(IUserService userService, IMapper mapper)
+		{
+			_userService = userService;
+			_mapper = mapper;
+		}
 
-        [HttpGet]
-        public async Task<IEnumerable<UserResponseModel>> GetAll()
-        {
-            var users = await _userService.GetAllAsync();
-            return _mapper.Map<IEnumerable<UserResponseModel>>(users);
-        }
-        [HttpGet("{id}")]
-        public async Task<UserResponseModel> Get(int id)
-        {
+		[HttpGet]
+		public async Task<IEnumerable<UserResponseModel>> GetAll()
+		{
+			var users = await _userService.GetAllAsync();
+			return _mapper.Map<IEnumerable<UserResponseModel>>(users);
+		}
+		[HttpGet("{id}")]
+		public async Task<UserResponseModel> Get(int id)
+		{
 			var user = _userService.GetByIdAsync(id);
-            return _mapper.Map<UserResponseModel>(user);
-        }
+			return _mapper.Map<UserResponseModel>(user);
+		}
 
-        [HttpPost]
-        public async Task<UserResponseModel> Post([FromBody] UserRequestModel userRequestModel)
-        {
-            var userMap= _mapper.Map<User>(userRequestModel);
-            var createdUser = await _userService.InsertAsnyc(userMap);
-            return _mapper.Map<UserResponseModel>(createdUser);
-        }
+		[HttpPost]
+		public async Task<UserResponseModel> Post([FromBody] UserRequestModel userRequestModel)
+		{
+			var userMap = _mapper.Map<User>(userRequestModel);
+			var createdUser = await _userService.InsertAsnyc(userMap);
+			return _mapper.Map<UserResponseModel>(createdUser);
+		}
 
-        [HttpPut]
-        public async Task<UserResponseModel> Put([FromBody] UserRequestModel userRequestModel)
-        {
+		[HttpPut]
+		public async Task<UserResponseModel> Put([FromBody] UserRequestModel userRequestModel)
+		{
 			var user = _mapper.Map<User>(userRequestModel);
 
 			var updatedUser = await _userService.UpdateAsnyc(user);
-            return _mapper.Map<UserResponseModel>(updatedUser);
-        }
+			return _mapper.Map<UserResponseModel>(updatedUser);
+		}
 
-        [HttpDelete("{id}")]
-        public async Task<UserResponseModel> Delete(int id)
-        {
-            var deletedUser = await _userService.DeleteAsync(id);
-            return _mapper.Map<UserResponseModel>(deletedUser);
-        }
-    }
+		[HttpDelete("{id}")]
+		public async Task<UserResponseModel> Delete(int id)
+		{
+			var deletedUser = await _userService.DeleteAsync(id);
+			return _mapper.Map<UserResponseModel>(deletedUser);
+		}
+	}
 }
---
+++ LMS.Data/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Data.Entities
+{
+	public class Course : BaseEntity
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int? TeacherId { get; set; }
+		public User Teacher { get; set; }
+	}
+}
